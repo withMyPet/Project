745e8e310467217b04e1becfdf827ae0b1604991
HeeSeok <nhs0912@nate.com>
2015-08-22 20:07:42 +0900

    heeseok

--- withMyPet/src/withMyPet/teset.java
+++ withMyPet/src/withMyPet/teset.java
@@ -7,10 +7,15 @@ public class teset {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		int b = 10;
+		System.out.println(b);
+
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(b);
 		list.add(20);
+		
+		System.out.println("---------");
 		System.out.println(list);
+		
 	}
 
 }
