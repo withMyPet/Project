aec719055968dc828bd41618eb8ddf41707e6419
HeeSeok <nhs0912@nate.com>
2015-08-22 09:06:57 +0900

    ArrayList - HeeSeok

--- withMyPet/src/withMyPet/teset.java
+++ withMyPet/src/withMyPet/teset.java
@@ -1,10 +1,17 @@
 package withMyPet;
 
+import java.util.ArrayList;
+
 public class teset {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
+		ArrayList<Integer> list = new ArrayList<Integer>();
 
+		list.add(a);
+		list.add(30);
+		System.out.println(list);
 	}
 
 }
