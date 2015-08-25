051dd1faaa717427362fb786f9dbf847b9b9933e
bakgyeore <grpark890112@gmail.com>
2015-08-25 15:02:01 +0900

    ㅜㅜ

--- withMyPet/src/vo/Animal_detail.java
+++ withMyPet/src/vo/Animal_detail.java
@@ -14,7 +14,39 @@ public class Animal_detail extends Animal_kind{
 		this.breed_code = breed_code;
 		this.breed_name = breed_name;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "Animal_detail [breed_code=" + breed_code + ", breed_name="
+				+ breed_name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + breed_code;
+		result = prime * result
+				+ ((breed_name == null) ? 0 : breed_name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal_detail other = (Animal_detail) obj;
+		if (breed_code != other.breed_code)
+			return false;
+		if (breed_name == null) {
+			if (other.breed_name != null)
+				return false;
+		} else if (!breed_name.equals(other.breed_name))
+			return false;
+		return true;
+	}
 }
